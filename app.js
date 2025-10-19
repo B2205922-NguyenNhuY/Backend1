3dd360bb3039046fad37e346496f99f649a1aa72
nhuy-git <yb2205922@student.ctu.edu.vn>
2025-10-20 02:28:31 +0700

    Cài đặt xử lý lỗi

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const ApiError = require("./app/api-error");
 
 const app = express();
 const contactsRouter = require("./app/routes/contact.route");
@@ -12,4 +13,16 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/contacts", contactsRouter);
+app.use((req, res, next) => {
+  // Khi không có route nào khớp với yêu cầu
+  next(new ApiError(404, "Resource not found"));
+});
+
+// Middleware xử lý lỗi tập trung
+app.use((error, req, res, next) => {
+  // Nếu không có statusCode, mặc định là 500 (Internal Server Error)
+  res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+  });
+});
 module.exports = app;
